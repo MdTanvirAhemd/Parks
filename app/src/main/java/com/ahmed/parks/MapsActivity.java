185b36eb2599447c30fac415b182e4852f0d1387
Overlord <tanvir1005@student.sust.edu>
2021-11-24 19:35:42 +0600

    4rd commit

--- app/src/main/java/com/ahmed/parks/MapsActivity.java
+++ app/src/main/java/com/ahmed/parks/MapsActivity.java
@@ -50,13 +50,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
 
         Repository.getParks((AsyncResponse) parks -> {
 
             for (Park park:parks) {
+                LatLng sydney = new LatLng(Double.parseDouble(park.getLatitude()), Double.parseDouble(park.getLongitude()));
+                mMap.addMarker(new MarkerOptions().position(sydney).title(park.getFullName()));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
                 Log.d("TTAG", "OnMapReady: " +park.getFullName());
 
             }
